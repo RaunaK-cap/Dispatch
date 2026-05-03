719f55c4aceedab2cce3c7331c12d912518c07b4
Raunak <raunakkumar6437@gmail.com>
2026-05-04 00:39:27 +0530

    backend routes

--- apps/api/controllers/content.ts
+++ apps/api/controllers/content.ts
@@ -9,6 +9,7 @@ export const config = async (req: Request, res: Response) => {
     try {
 
         const resp = await prisma.channelconfig.upsert({
+
             where: {
                 channel_USERID: {
                     channel: channel,
@@ -41,6 +42,34 @@ export const config = async (req: Request, res: Response) => {
 
 }
 
+export const delete_config = async (req: Request, res: Response) => {
+
+    const { id } = req.body
+
+    try {
+
+        const resp = await prisma.channelconfig.delete({
+            where: {
+                id: id
+            }, select: {
+                channel: true,
+                config: true
+            }
+        })
+
+        res.status(200).json({
+            message: "deleted",
+            resp
+        })
+
+    } catch (error) {
+        res.json({
+            message: "eerror while deleting",
+            error
+        })
+    }
+}
+
 
 export const notification_message = async (req: Request, res: Response) => {
 
@@ -68,11 +97,37 @@ export const notification_message = async (req: Request, res: Response) => {
 
 }
 
+export const delete_notification_message = async (req: Request, res: Response) => {
+
+    const { id } = req.body
+    try {
+        const resp = await prisma.notificationmessage.delete({
+            where: {
+                id: id
+            }, select: {
+                deliverylogs: true
+            }
+        })
+
+        res.status(200).json({
+            message: "deleted",
+            resp
+        })
+    } catch (error) {
+        res.status(200).json({
+            message: "error while deleting"
+        })
+    }
+}
+
 export const deliverylogs = async (req: Request, res: Response) => {
+
     const { messageID, channel, status, retrycount, redisjobID, latencyMS } = req.body
+
     try {
         const response = await prisma.deliverylog.create({
             data: {
+
                 USERID: Number(req.userID),
                 messageID: messageID,
                 channel: channel,
@@ -88,6 +143,7 @@ export const deliverylogs = async (req: Request, res: Response) => {
             message: "logs have been submitted ",
             response
         })
+
     } catch (error) {
         res.status(400).json({
             message: "error while saving logs"
--- apps/api/routes/usercontrol.ts
+++ apps/api/routes/usercontrol.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import usermiddleware from "../middleware/middleware";
-import { config, deliverylogs, notification_message } from "../controllers/content";
+import { config, delete_config, delete_notification_message, deliverylogs, notification_message } from "../controllers/content";
 
 const usercontent = Router()
 
 usercontent.post("/config", usermiddleware, config)
+usercontent.delete("/config/delete", usermiddleware, delete_config)
+
 usercontent.post("/notification", usermiddleware, notification_message)
+usercontent.delete("/notification/delete", usermiddleware, delete_notification_message)
+
 usercontent.post("/deliverylogs", usermiddleware, deliverylogs)
 
 export default usercontent
