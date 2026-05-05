55d80c0293054f2e3230782409ad6bc9918e4dbe
Raunak <raunakkumar6437@gmail.com>
2026-05-05 23:12:06 +0530

    idk forgot

--- packages/redis/index.ts
+++ packages/redis/index.ts
@@ -1,5 +1,4 @@
-import express from "express"
-import { createClient } from "redis"
+import { createClient, RootNodesUnavailableError } from "redis"
 import dotenv from "dotenv"
 
 dotenv.config()
@@ -8,6 +7,34 @@ dotenv.config()
 const client = createClient().on("error", (err) =>
     console.log("Redis Client Error", err),
 );
+
 await client.connect();
 
 
+export async function xaddbulk(notificationData: { channelName: string; key: string, }[]) {
+    const pipeline = client.MULTI();
+
+    RootNodesUnavailableError.forEach((data) => {
+        pipeline.xAdd(
+            "NOTIFY",
+            "*",
+            {
+                url: data.url,
+                id: data.id.toString(),
+            },
+            {
+                TRIM: {
+                    strategy: "MAXLEN",
+                    strategyModifier: "~",
+                    threshold: 50,
+                },
+            },
+        );
+    });
+
+    const results = await pipeline.EXEC();
+    console.log(
+        `[${new Date().toISOString()}] Added ${results?.length} entries:`,
+        results,
+    );
+}
